b9537e75f4227bd7362d1c42c984caa494bc890b
JM <jm.jacquier@gmail.com>
2022-08-18 10:16:14 +0200

    ok

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-require("dotenv").config(); // this line is mandatory when you want to read variables from the '.env' file
-console.log(`The server will run on port ${process.env.SERVER_PORT}`);
+require("dotenv").config();
+
 console.log(
-  `The secret key ${process.env.SECRET_API_KEY} will be used to connect to the third party API`
-);
-console.log(
-  `The server will connect to the database with the user "${process.env.DB_USER}" and the password "${process.env.DB_PASSWORD}"`
+  `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY} AND I LOVE ${process.env.MY_LANGUAGE}`
 );
